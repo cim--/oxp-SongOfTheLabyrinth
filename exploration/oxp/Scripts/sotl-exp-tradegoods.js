c16682cd387d4ec557dcd512a93c7ffd074501b5
cim <cim@oolite.org>
2015-07-10 15:19:26 +0100

    Add trade goods file to exploration
    (No quantities or use yet - not yet implemented)

---
+++ exploration/oxp/Scripts/sotl-exp-tradegoods.js
@@ -0,0 +1,19 @@
+"use strict";
+
+this.name = "SOTL Exploration Trade Goods";
+
+this.updateGeneralCommodityDefinition = function(marketdef, station, system) {
+	log(this.name,marketdef.name,station,system);
+	if (station) { return marketdef; }
+	if (system != 0) {
+		marketdef.price = 0;
+		marketdef.quantity = 0;
+		return marketdef;
+	}
+	
+	/* For now */
+	marketdef.price = 0;
+	marketdef.quantity = 0;
+	return marketdef;
+}
+	
